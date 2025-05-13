69481e74efe15aaa12c5ae01287d555b2a4e7b13
Umakanth <umakanthreddy055@gmail.com>
2025-05-13 19:48:45 +0530

    modified:   modules/hyprland.nix

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -71,17 +71,80 @@
         gaps_out = 10;
         border_size = 0;
       };
+      misc = {
+        layers_hog_keyboard_focus = true;
+        initial_workspace_tracking = 0;
+        mouse_move_enables_dpms = true;
+        key_press_enables_dpms = false;
+        disable_hyprland_logo = true;
+        disable_splash_rendering = true;
+        enable_swallow = false;
+        vfr = true;   # Variable Frame Rate 
+        vrr = 2;   #Variable Refresh Rate  Might need to set to 0 for NVIDIA/AQ_DRM_DEVICES
+        # Screen flashing to black momentarily or going black when app is fullscreen
+        # Try setting vrr to 0
+      };  
+
+
+      dwindle = {
+        pseudotile = true;
+        preserve_split = true;
+      };
+
       decoration = {
+        rounding = 10;
         blur = {
           enabled = true;
-          size = 20;
-          passes = 1;
-          ignore_opacity = true;
+          size = 5;
+          passes = 3;
+          ignore_opacity = false;
           new_optimizations = true;
-          popups = true;
-          xray = true;
+        };
+        shadow = {
+          enabled = true;
+          range = 4;
+          render_power = 3;
+          color = "rgba(1a1a1aee)";
         };
       };
+
+      cursor = {
+        sync_gsettings_theme = true;
+        no_hardware_cursors = 2; # change to 1 if want to disable
+        enable_hyprcursor = false;
+        warp_on_change_workspace = 2;
+        no_warps = true;
+      };
+
+      render = {
+        explicit_sync = 1; # Change to 1 to disable
+        explicit_sync_kms = 1;
+        direct_scanout = 0;
+      };
+
+      master = {
+        new_status = "master";
+        new_on_top = 1;
+        mfact = 0.5;
+      };
+      env = [
+        "NIXOS_OZONE_WL, 1"
+        "NIXPKGS_ALLOW_UNFREE, 1"
+        "XDG_CURRENT_DESKTOP, Hyprland"
+        "XDG_SESSION_TYPE, wayland"
+        "XDG_SESSION_DESKTOP, Hyprland"
+        "GDK_BACKEND, wayland, x11"
+        "CLUTTER_BACKEND, wayland"
+        "QT_QPA_PLATFORM=wayland;xcb"
+        "QT_WAYLAND_DISABLE_WINDOWDECORATION, 1"
+        "QT_AUTO_SCREEN_SCALE_FACTOR, 1"
+        "SDL_VIDEODRIVER, x11"
+        "MOZ_ENABLE_WAYLAND, 1"
+        "AQ_DRM_DEVICES,/dev/dri/card0:/dev/dri/card1"
+        "GDK_SCALE,1"
+        "QT_SCALE_FACTOR,1"
+        "EDITOR,nvim"
+      ];
       layerrule = [
         "blur, wlogout"
         "blur, kitty"
