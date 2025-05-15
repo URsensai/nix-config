86b395d9d7edccaf90515eff5e33a6a9064722cd
Umakanth <umakanthreddy055@gmail.com>
2025-05-15 23:24:16 +0530

    modified:   modules/starship.nix

--- modules/starship.nix
+++ modules/starship.nix
@@ -12,8 +12,8 @@
       };
 
       character = {
-        success_symbol = "[❯](bold green)";
-        error_symbol = "[❯](bold red)";
+        success_symbol = "[ ❯ ](bold green)";
+        error_symbol = "[ ❯ ](bold red)";
       };
     };
   };
